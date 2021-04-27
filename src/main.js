4b338aed740157f0e94eb00d9c7687d37b382bf7
lllxxl <1808791317@qq.com>
2021-04-28 00:28:55 +0800

    add api_url

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ export default function (Vue, { router, head, isClient }) {
   Vue.mixin({
     data(){
       return {
-        GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
+        GRIDSOME_API_URL: 'http://106.75.108.252:1337'
       }
     }
   })
